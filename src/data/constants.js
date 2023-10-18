8166e06bf28b99ae61072cba3dfe9548ab8dbce9
Riddhesh <riddheshkarnik21@gmail.com>
2023-10-18 18:11:23 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,168 @@
+export const Bio = {
+    name: "Riddhesh Karnik",
+    roles: [
+      "Full Stack Developer",
+      "Android Developer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/justriddhesh81",
+    linkedin: "https://www.linkedin.com/in/riddhesh-karnik-571161251/",
+    
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        
+        {
+          name: "Flutter",
+          image:
+            "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        
+        {
+          name: "Adobe XD",
+          image:
+            "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+        },
+
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://vtopcc.vit.ac.in/vtop/assets/img/VITLogoEmblem.png",
+      school: "Vellore Institute Of Technology, Chennai",
+      date: "Sept 2021 - May 2025",
+      grade: "8.65 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Vellore Institute of Technology, Chennai. I have completed 5 semesters and have a CGPA of 8.65. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Business Innovation Community(BIC) at VIT, where I am learning and working on exciting projects with a team of talented and skilled minds.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://tse3.mm.bing.net/th?id=OIP.4Uco96vYw8OD54zA6bsbcQAAAA&pid=Api&P=0&h=180",
+      school: "Doon International Public School, Ahmedabad",
+      date: "Apr 2020 - Apr 2021",
+      grade: "84%",
+      desc: "I completed my class 12 high school education at Doon International Public School, Ahmedabad, where I studied Science with Mathematics.",
+      degree: "Gujarat Board(XII)",
+    },
+    {
+      id: 2,
+      img: "https://tse3.mm.bing.net/th?id=OIP.4Uco96vYw8OD54zA6bsbcQAAAA&pid=Api&P=0&h=180",
+      school: "Doon International Public School, Ahmedabad",
+      date: "Apr 2018 - Apr 2019",
+      grade: "89%",
+      desc: "I completed my class 10 education at Doon International Public School, Ahmedabad.",
+      degree: "Gujarat Borad(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      
+    },
+  ];
+  
+  export const TimeLineData = [
+    
+  ];
